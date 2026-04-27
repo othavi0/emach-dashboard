8885f403dda5a7be451ef0f968e8ac1d06b4ba58
Othavio Quiliao <othavioaqa@gmail.com>
2026-04-27 09:32:23 -0300

    refactor(db): schema/index.ts barrel adiciona categories + consent-log

--- packages/db/src/schema/index.ts
+++ packages/db/src/schema/index.ts
@@ -1,7 +1,9 @@
 // biome-ignore lint/performance/noBarrelFile: intentional public API barrel for @emach/db schema consumers
 export * from "./api-keys";
 export * from "./auth";
+export * from "./categories";
 export * from "./client";
+export * from "./consent-log";
 export * from "./inventory";
 export * from "./promotions";
 export * from "./stock-movements";
